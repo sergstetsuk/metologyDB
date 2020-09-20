bdb95562edfb77c729796d6013fdb096619bf093
F1rset <stekostant@gmail.com>
2020-09-20 10:28:58 +0300

    offline versions of jquery and jsgrid

--- cathedras/index.php
+++ cathedras/index.php
@@ -16,7 +16,12 @@ switch($_SERVER["REQUEST_METHOD"]) {
 
  */
 header("Content-Type: application/json");
-echo "[{\"id\":0, \"name\":\"abc\"}]";
+echo "[";
+echo "{\"id\":1, \"name\":\"abc1\"},";
+echo "{\"id\":2, \"name\":\"abc2\"},";
+echo "{\"id\":3, \"name\":\"abc3\"},";
+echo "{\"id\":4, \"name\":\"abc4\"}";
+echo "]";
 /*
  * echo json_encode($result);
  */
